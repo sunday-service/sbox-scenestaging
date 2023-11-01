7a0002c5e5531f9e2da0de9765cfe244a2cc2dd1
walker407 <hscott407@gmail.com>
2023-11-01 18:38:24 -0500

    Testing projectile impact...

---
+++ code/WizardsComponents/Entities/Weapons/Projectiles/ProjectileImpact.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+
+public sealed class ProjectileImpact : BaseComponent, BaseComponent.ICollisionListener
+{
+	[Property] public GameObject ImpactParticles { get; set; }
+
+	public void OnCollisionStart( Collision o )
+	{
+		if(ImpactParticles is not null)
+		{
+			SceneUtility.Instantiate( ImpactParticles, o.Contact.Point );
+		}
+	}
+
+	public void OnCollisionStop( CollisionStop other )
+	{
+		GameObject.Destroy();
+	}
+
+	public void OnCollisionUpdate( Collision other )
+	{
+
+	}
+}
