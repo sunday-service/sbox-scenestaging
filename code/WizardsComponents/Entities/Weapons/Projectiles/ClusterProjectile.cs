32244ce15b14d86602087ccad64c2a59745f167f
walker407 <hscott407@gmail.com>
2023-11-02 11:01:28 -0500

    Updating projectiles. Adding cluster projectile. Adding delayed explosion / cluster projectile components. Updating projectile test scene

---
+++ code/WizardsComponents/Entities/Weapons/Projectiles/ClusterProjectile.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox;
+
+public sealed class ClusterProjectile : BaseComponent, BaseComponent.ICollisionListener
+{
+	[Property] public GameObject Projectile { get; set; }
+	[Property] public int NumberOfProjectiles { get; set; } = 3;
+	[Property] public float ProjectileSpread { get; set; } = 128f;
+	
+	public void OnCollisionStart( Collision o )
+	{
+		if ( Projectile is not null )
+		{
+			for ( int i = 0; i < NumberOfProjectiles; i++ )
+			{
+				var random = new Vector3( Game.Random.Float( -1f, 1f ), Game.Random.Float( -1f, 1f ), Game.Random.Float( 0.5f, 1f ) );
+				var direction = (Vector3.Up * Game.Random.Float( 0.1f, 1f )) + (random * Game.Random.Float( 1f, 1.5f ));
+
+				var projectile = SceneUtility.Instantiate( Projectile, o.Contact.Point + (direction));
+				projectile.GetComponent<PhysicsComponent>().Velocity = direction * ProjectileSpread;
+				
+			}
+		}
+
+		GameObject.Destroy();
+	}
+
+	public void OnCollisionStop( CollisionStop other )
+	{
+		
+	}
+
+	public void OnCollisionUpdate( Collision other )
+	{
+
+	}
+}
---
+++ code/WizardsComponents/Entities/Weapons/Projectiles/DelayedExplosion.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+
+public sealed class DelayedExplosion : BaseComponent
+{
+	[Property] public GameObject ExplosionParticles { get; set; }
+	[Property] float Seconds { get; set; }
+
+	TimeUntil timeUntilDie;
+
+	public override void OnEnabled()
+	{
+		timeUntilDie = Seconds;
+	}
+
+	public override void Update()
+	{
+		if ( timeUntilDie <= 0.0f )
+		{
+			if ( ExplosionParticles is not null )
+			{
+				SceneUtility.Instantiate( ExplosionParticles, GameObject.Transform.Position );
+			}
+
+			GameObject.Destroy();
+		}
+	}
+}
